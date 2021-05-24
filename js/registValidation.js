c51d21ad82fd38764017c2e898a7d6029dc58076
Mcooldown <vincenthadinata30@gmail.com>
2021-05-24 21:56:28 +0700

    validate js

--- js/registValidation.js
+++ js/registValidation.js
@@ -1,7 +1,65 @@
 
 function validate(form){
-    // alert(form.fullname.value);
-    var gender = form.gender.value;
-    alert(gender);
-    return false;
+    if(form.fullname.value == ""){
+        alert("Name must be filled");
+        return false;
+        
+    }else if(form.fullname.value.length < 5 || form.fullname.value.length > 20){
+        alert("Name must between 5-20 characters");
+        return false;
+
+    }else if(form.email.value == ""){
+        alert("email must be filled");
+        return false;
+
+    }else if(!form.email.value.endsWith("@gmail.com")){
+        alert("email format must ends with @gmail.com");
+        return false;
+    
+    }else if (form.username.value == ""){
+        alert("username must be filled");
+        return false;
+    
+    }else if(form.username.value.length < 5 || form.username.value.length > 20){
+        alert("Username must between 5-20 characters");
+        return false;
+
+    }else if(form.gender.value == ""){
+        alert("gender must be selected");
+        return false;
+    
+    }else if(form.age.value == ""){
+         alert("age must be filled");
+         return false;
+    }
+    else if(isNaN(form.age.value)){
+        alert("age must be a number");
+        return false;
+    }
+    else if(form.age.value < 1){
+        alert("age must greater than 0");
+        return false;
+
+    }else if(form.password.value == ""){
+        alert("password must be filled");
+        return false;
+    
+    }else if(form.password.value.length < 8){
+        alert("password length must minimum 8 characters");
+        return false;
+    
+    }else if(form.confirm_password.value == ""){
+        alert("confirm password must be filled");
+        return false;
+
+    }else if(form.password.value != form.confirm_password.value){
+        alert("password not match with confirm password");
+        return false;
+
+    }else if(!form.agreeterms.checked){
+        alert("please check the checkbox");
+        return false;
+    }
+    
+    return true;
 }
