5d31bc67f84aac5668f89de587d18a6b70722641
Ricky <phangr28@gmail.com>
2021-05-24 21:12:55 +0700

    fix testimonial

--- js/registValidation.js
+++ js/registValidation.js
@@ -1,5 +1,17 @@
 
 function validate(form){
-    alert(form.fullname.value);
+    var fullname = form.fullname.value;
+    var email = form.email.value;
+    var username = form.username.value;
+    var gender = form.gender.value;
+    var age = form.age.value;
+    var password = form.password.value;
+    var repassword = form.repassword.value;
+    var agreeterms = form.agree.checked;
+
+
+    alert(gender)
+
+
     return false;
 }
