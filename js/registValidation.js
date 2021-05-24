8071f8d1aa64a2f47616ffaf41d69b68c53b35be
Ricky <phangr28@gmail.com>
2021-05-24 20:32:41 +0700

    change regist

---
+++ js/registValidation.js
@@ -0,0 +1,13 @@
+
+function asd(){
+    var fullname = document.getElementById("fullname").value;
+    var email = document.getElementById("email").value;
+    var username = document.getElementById("usernmae").value;
+    var gender = document.getElementById("gender").value;
+    var age = document.getElementById("age").value;
+    var password = document.getElementById("password").value;
+    var repassword = document.getElementById("repassword").value;
+    var agreeterms = document.getElementById("agreeterms").value;
+
+    console.log(fullname);
+}
