df494a4ce4f0fc03fb8be53eb5fdae9b66149735
Mcooldown <vincenthadinata30@gmail.com>
2021-05-24 20:49:56 +0700

    form

--- js/registValidation.js
+++ js/registValidation.js
@@ -1,13 +1,5 @@
 
-function asd(){
-    var fullname = document.getElementById("fullname").value;
-    var email = document.getElementById("email").value;
-    var username = document.getElementById("usernmae").value;
-    var gender = document.getElementById("gender").value;
-    var age = document.getElementById("age").value;
-    var password = document.getElementById("password").value;
-    var repassword = document.getElementById("repassword").value;
-    var agreeterms = document.getElementById("agreeterms").value;
-
-    console.log(fullname);
+function validate(form){
+    alert(form.fullname.value);
+    return false;
 }
