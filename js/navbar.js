a758eb07bb793852f2fcd35ac057a2867f5cc4bf
Mcooldown <vincenthadinata30@gmail.com>
2021-06-03 21:41:51 +0700

    change navbar in html

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,9 @@
 function showSidebar(){
      document.getElementById("sidebar").style.display = "block";
+     document.getElementById("main-content").style.display = "none";
 }
 
 function closeSidebar(){
      document.getElementById("sidebar").style.display = "none";
+     document.getElementById("main-content").style.display = "block";
 }
