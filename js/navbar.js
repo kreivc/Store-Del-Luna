bfe36ff6d0fd0f073692dfb9976f773fca4ec59f
Ricky <phangr28@gmail.com>
2021-06-14 20:43:12 +0700

    fix nav and detail

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,11 @@
+
+window.addEventListener("resize", function() {
+     if (window.matchMedia("(max-width: 600px)").matches) {
+          document.getElementById("sidebar").style.display = "none";
+     }
+})
+
+
 function showSidebar(){
      document.getElementById("sidebar").style.display = "block";
 }
